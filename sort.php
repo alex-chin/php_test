de0f22ffcac6f4a183a2c952434580c98a98f2a5
Aleksandr Klyanchin <k142@mail.ru>
2025-05-12 23:10:44 +0300

    sort

---
+++ sort.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// обмен элемента i c элементом j в массиве nums
+function obmen(&$nums, $i, $j): void
+{
+    [$nums[$i], $nums[$j]] = [$nums[$j], $nums[$i]]; // Обмен в одну строку
+}
+
+// сортировка массива 1 проход
+function b_sort($nums): array
+{
+    $n = count($nums);
+    foreach (range(0, $n - 1) as $j) {
+        $is_changed = false;
+        for ($i = 0; $i < $n - 1 - $j; $i++) {
+            if ($nums[$i] > $nums[$i + 1]) {
+                obmen($nums, $i, $i + 1);
+                $is_changed = true;
+            }
+        }
+        if (!$is_changed)
+            break;
+    }
+    return $nums;
+}
+
+
+$nums = [3, 4, 1, 2, 3];
+print_r(b_sort($nums));
